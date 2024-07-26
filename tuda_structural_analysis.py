443726c55d271d7e2a484bd853d7b3b079da6c4e
AKRAM METAR <akrammetar01@gmail.com>
2024-07-26 19:30:25 +0200

    Initialisation

---
+++ tuda_structural_analysis.py
@@ -0,0 +1,2 @@
+# Created on 26.07.2024 at Akaflieg Darmstadt, Author @ akram_metar@akaflieg.tu-darmstadt.de
+# This is the official file containing all the functions and codes related to the structural analysis
